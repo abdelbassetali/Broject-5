130369cc168a07f80869f58aba5411f1d2b34d42
abdelbassetali <146743316+abdelbassetali@users.noreply.github.com>
2026-08-30 13:44:43 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -41,13 +41,11 @@ const product2=new products('Watch','150 $','Phone  ','images/product-2.jpg',2);
 const product3=new products('Earpods','120 $','Phone  ','images/product-3.jpg',3);
 const product4=new products('Handsfree ','50 $','Phone ','images/product-4.jpg',4);
 const product5=new products('Laptop','350 $','Phone ','images/product-5.jpg',5);
-const product6=new products('Laplet','150 $','Phone','images/product-6.jpg',6);
+const product6=new products('Taplet','150 $','Phone','images/product-6.jpg',6);
 const product7=new products('Tv',' 410 $','Appliances','images/product-7.jpg',7);
 const product8=new products('Playstaion-5','280 $','Gaming ','images/product-8.jpg',8);
 const product9=new products('Controller','20 $','Gaming ','images/product-9.jpg',9);
 
-// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-// //////رسم المنتجات 
 
 function draw(){
 for(let i =1;i<product.length;i++){
@@ -64,7 +62,7 @@ for(let i =1;i<product.length;i++){
                         <a href="#" style="margin-left: auto; display: inline-block;" ><i class="fas fa-heart" style="font-size: 1.5rem;" id="${product[i].id}00000" onclick='addFavourite(this.id)'></i></a>
                     </div> 
             </div>
-    </div> <!-- End of card -->
+    </div> 
     `
 }
 }
@@ -87,7 +85,6 @@ function addItems(id){
      
     }
 }
-// /////////////علشان اعمل ازالة للمنتج 
 
 function removeItems(id){
     let removeItem= document.getElementById(id)
